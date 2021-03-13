2b4a5dc4bd43675ec4e0232e755999a25ec79d09
Jefferson <jefry04@gmail.com>
2021-03-12 19:53:14 -0500

    ad compare the numbers

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-$(document).ready(function () {
-  var number = Math.floor(1000 + Math.random() * 9000);
-  console.log(number);
+var randomNumber = Math.floor(1000 + Math.random() * 9000);
+var randomArray = randomNumber.toString().split('');
+console.log(randomNumber);
+console.log(randomArray);
+
+$('form').on('submit', function (e) {
+  e.preventDefault();
+  var numbers = $('#numbers').val();
+  var picas = 0;
+  var fijas = 0;
+  var numbersArray = numbers.toString().split('');
+
+  console.log(numbersArray);
+
+  randomArray.forEach(function (item, index) {
+    if (item === numbersArray[index]) {
+      fijas++;
+    } else if (numbersArray.indexOf(item) !== -1) {
+      picas++;
+    }
+  });
+
+  if (numbers.toString().length === 4) {
+    if (numbers == randomNumber) {
+      alert('se acabo el juego');
+      // CREAR FUNCION PARA INICIALIZAR  JUEGO
+    }
+    $('tbody').append(
+      '<tr>' +
+        '<td>' +
+        numbers +
+        '</td>' +
+        '<td>' +
+        picas +
+        '</td>' +
+        '<td>' +
+        fijas +
+        '</td>' +
+        '</tr>'
+    );
+  } else alert('escribiste mas o menos de 4 numeros');
 });
