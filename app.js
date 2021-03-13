210178f5f512daaa5e656ffa3871d555c9d24129
Jefferson <jefry04@gmail.com>
2021-03-12 23:31:53 -0500

    ad modal and render function

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-var randomNumber = Math.floor(1000 + Math.random() * 9000);
-var randomArray = randomNumber.toString().split('');
-console.log(randomNumber);
-console.log(randomArray);
+var randomArray = [];
+
+function createNumber() {
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var number = [];
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  for (var i = 1; i <= 4; i++) {
+    number.push(
+      numbers.splice(getRandomInt(0, numbers.length), 1).pop().toString()
+    );
+  }
+  console.log(number.join(' '));
+  randomArray = number;
+}
+function render() {
+  location.reload();
+}
+
+function ganaste() {
+  $('#myModal').modal();
+}
+$('#start').on('click', function () {
+  render();
+});
 
 $('form').on('submit', function (e) {
   e.preventDefault();
@@ -9,8 +31,7 @@ $('form').on('submit', function (e) {
   var picas = 0;
   var fijas = 0;
   var numbersArray = numbers.toString().split('');
-
-  console.log(numbersArray);
+  var test = /([0-9]).*?\1/.test(numbers); // verifica si hay numeros repetidos en el numero ingresado en el form
 
   randomArray.forEach(function (item, index) {
     if (item === numbersArray[index]) {
@@ -20,11 +41,12 @@ $('form').on('submit', function (e) {
     }
   });
 
-  if (numbers.toString().length === 4) {
-    if (numbers == randomNumber) {
-      alert('se acabo el juego');
-      // CREAR FUNCION PARA INICIALIZAR  JUEGO
+  if (numbers.toString().length === 4 && !test) {
+    if (fijas === 4) {
+      ganaste();
     }
+    $('#numbers').parent().removeClass('has-error');
+    $('p').html('Ingresa cuatro numeros diferentes y oprime enter');
     $('tbody').append(
       '<tr>' +
         '<td>' +
@@ -38,5 +60,18 @@ $('form').on('submit', function (e) {
         '</td>' +
         '</tr>'
     );
-  } else alert('escribiste mas o menos de 4 numeros');
+  } else {
+    $('#numbers').parent().addClass('has-error');
+    $('p').html(
+      'escriba ' +
+        '<b class="text-danger">' +
+        ' cuatro numeros diferentes ' +
+        '</b>' +
+        ' y presione enter'
+    );
+  }
+});
+
+$(document).ready(function () {
+  createNumber();
 });
